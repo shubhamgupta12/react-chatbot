382b63aacd0d20efef587544efdab378f48ed3aa
Shubham Gupta <shubham.gupta@srijan.net>
2020-11-06 16:57:21 +0530

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-`npm install` and then `npm start`.
-
-Note: Node must be installed on the system.
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// Import dependencies
-import { combineReducers } from "redux";
-
-// Import reducers
-import { chatbot } from "./chatbot";
-
-// Export combined reducers
-export default combineReducers({ chatbot });
